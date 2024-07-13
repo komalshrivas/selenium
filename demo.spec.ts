aff4a9498e7d471725751b20b73edc619bdd32f0
komalshrivas <39797917+komalshrivas@users.noreply.github.com>
2024-07-13 14:36:39 +0530

    Add files via upload

---
+++ demo.spec.ts
@@ -0,0 +1,32 @@
+// let number: number[]=[1,2,3,4,5,6]
+// for(let i=0; i<number.length; i++){
+//     console.log(number[i])
+// }
+  
+  
+// //====================================
+//  let number: number[]= [1,2,3,4,5,6,7,8,9]
+//  console.log(number)
+//  //====================
+//  let numbers: number[] = [1, 2, 3, 4, 5];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+// //============================
+// let greeting: string = "Hello, TypeScript!";
+// console.log(greeting);
+// //===========================================
+ 
+console.log("this is komal shrivas")
+
+let a= 4
+console.log(typeof(a))
+
+let b = "komal"
+console.log(typeof(b))
+
+let required = true
+console.log(!required)
+
+//==================
+//nul and undefined
