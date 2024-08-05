625fa4c8854ee55064330607e8d9159bd6895b2c
komalshrivas <shrivas.komal1990@gmail.com>
2024-08-05 15:11:52 +0530

    my first code

---
+++ demo2.spec.ts
@@ -0,0 +1,22 @@
+// const flag = true
+
+// if(!flag){
+
+// console.log("this is correct")
+// }
+// else{
+
+// console.log("not correct")
+// }
+//=================
+    
+let i = 0
+while(i<10){
+    i++
+    console.log(i)
+}
+//====================
+
+let number: number[]= [1,2,3,4,5,6,7,8,9]
+  console.log(number)
+
