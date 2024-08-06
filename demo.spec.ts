5d27129352e377a736153f83aaee9c733a646951
komalshrivas <shrivas.komal1990@gmail.com>
2024-08-06 11:36:55 +0530

    second commit

--- demo.spec.ts
+++ demo.spec.ts
@@ -2,7 +2,7 @@
 // for(let i=0; i<number.length; i++){
 //     console.log(number[i])
 // }
-  
+//Hello
   
 // //====================================
 //  let number: number[]= [1,2,3,4,5,6,7,8,9]
